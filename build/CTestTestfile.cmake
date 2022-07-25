c0e542c8e35120d173f319b9121dc9ae896db383
Bhagath M <33441778+bhagath555@users.noreply.github.com>
2022-07-25 09:18:29 +0530

    successful run of the test.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/kisho/Dropbox (Personal)/PC/Documents/GitHub/learn-cpp-gtest
+# Build directory: C:/Users/kisho/Dropbox (Personal)/PC/Documents/GitHub/learn-cpp-gtest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[AllFactTest]=] "unittest")
+set_tests_properties([=[AllFactTest]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/kisho/Dropbox (Personal)/PC/Documents/GitHub/learn-cpp-gtest/CMakeLists.txt;12;add_test;C:/Users/kisho/Dropbox (Personal)/PC/Documents/GitHub/learn-cpp-gtest/CMakeLists.txt;0;")
